605981fc843a9b96d32a9f5b22c90083238ac605
Janek9709 <jan.biniek97@gmail.com>
2023-03-06 17:14:44 +0100

    Inital project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
